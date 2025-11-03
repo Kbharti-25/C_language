fb7da99f7cf7a9dd3510c7487c9b3fccac41846c
Kbharti-25 <kbharti0625@gmail.com>
2025-11-03 19:27:00 +0530

    tof find the combination of function

---
+++ combination_function.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int factorial(int x){
+    int fact=1;
+    for(int i=2;i<=x;i++){
+        fact=fact*i;
+
+    }
+    return fact;
+
+
+}
+int main(){
+    int n;
+    int r;
+    int c;
+    printf("enter n in ncr:");
+    scanf("%d",&n);
+    printf("enter r in ncr:");
+    scanf("%d",&r);
+    c=n-r;
+    int nfact=factorial(n);
+    int rfact=factorial(r);
+    int nrfact=factorial(c);
+    int ncr=nfact/(rfact*nrfact);
+    printf("%d",ncr);
+    return 0;
+}
